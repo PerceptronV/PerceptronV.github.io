03bef3cc69d7f1db2961b0ff70d722b429a06d57
PerceptronV <neutrinovs@gmail.com>
2021-04-06 20:39:37 +0800

    Update render.js

--- beta/model-viewer/render.js
+++ beta/model-viewer/render.js
@@ -5,31 +5,33 @@ import { OrbitControls } from './lib/three/examples/jsm/controls/OrbitControls.j
 const positions = {
     camera: {
         x: 0,
-        y: 40,
+        y: 20,
         z: 30
     },
     model: {
         x: 0,
-        y: 0,
-        z: -10
+        y: -10,
+        z: -11.25
     }
 }
+
+const USEGRID = false;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
 scene.add(camera);
-camera.position.set( positions.camera.x, positions.camera.y, positions.camera.z );
-camera.lookAt( positions.model.x, positions.model.y, positions.model.z );
 
 const light = new THREE.AmbientLight( 0x404040, 3.5 );
 scene.add( light );
 
-var gridHelper = new THREE.GridHelper(100, 10);
-scene.add(gridHelper);
+if (USEGRID) {
+    var gridHelper = new THREE.GridHelper( 100, 10 );
+    scene.add( gridHelper );
+}
 
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor( 0xffffff, 0);
+renderer.setClearColor( 0xffffff, 0 );
 document.body.appendChild( renderer.domElement );
 
 const controls = new OrbitControls( camera, renderer.domElement );
@@ -42,6 +44,7 @@ loader.load( "models/ketelr.glb", ( gltf ) => {
 	console.error( error );
 } );
 
+camera.position.set( positions.camera.x, positions.camera.y, positions.camera.z );
 controls.update();
 
 const animate = function () {
