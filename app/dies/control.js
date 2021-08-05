516eae325e18ad0bdab0a1a7a27e1581248c715a
PerceptronV <neutrinovs@gmail.com>
2021-08-05 17:52:29 +0100

    Updated Dies with Bootstrap

--- app/dies/control.js
+++ app/dies/control.js
@@ -2,11 +2,24 @@ function str2bool(str) {
     return str == "true";
 }
 
+function replace_space(str){
+    return str.replace(/\s+/g, "_");
+}
+
+function update_progbars(){
+    var keys = Object.keys(BARPROGS);
+    for (var i in keys){
+        var prog = Math.round(BARPROGS[keys[i]] / BARTOTS[keys[i]] * 100);
+        document.getElementById(keys[i]).style = "width: " + prog + "%;";
+    }
+}
+
 class Checklist {
-    constructor(name, template, element_id) {
+    constructor(name, template, element_id, bar_id) {
         this.name = name;
         this.template = template;
         this.element_id = element_id;
+        this.bar_id = bar_id;
     }
 
     fillstatus() {
@@ -14,7 +27,7 @@ class Checklist {
         var keys = Object.keys(this.template);
     
         for (var i in keys) {
-            var cache = localStorage.getItem(this.name + keys[i]);
+            var cache = localStorage.getItem(this.name + replace_space(keys[i]));
             copy[keys[i]] = cache != null ? str2bool(cache) : this.template[keys[i]];
         }
         return copy;
@@ -24,42 +37,58 @@ class Checklist {
         var tasks = this.fillstatus();
         var list = document.getElementById(this.element_id);
         var keys = Object.keys(this.template);
+        BARPROGS[this.bar_id] = 0;
+        BARTOTS[this.bar_id] = keys.length;
     
         for (var i in keys) {
-            var element = document.createElement('li');
-    
+            var identifier = this.name + replace_space(keys[i]);
+
+            var element = document.createElement('label');
+                element.className = "form-check list-group-item";
+
             var checkbox = document.createElement('input');
                 checkbox.type = "checkbox";
-                checkbox.name = this.name + keys[i];
-                checkbox.value = this.name + keys[i];
-                checkbox.id = this.name + keys[i];
+                checkbox.className = "form-check-input checkbox-offset";
+                checkbox.name = identifier;
+                checkbox.value = identifier;
+                checkbox.dataset.bar_id = this.bar_id;
+                checkbox.id = identifier;
                 checkbox.checked = tasks[keys[i]];
+
+                if (tasks[keys[i]]) BARPROGS[this.bar_id] += 1;
                 
                 checkbox.onclick = function(){
-                    localStorage.setItem(this.name, String(document.getElementById(this.name).checked));
+                    localStorage.setItem(this.name, this.checked);
+                    BARPROGS[this.dataset.bar_id] += this.checked ? 1 : -1;
+                    update_progbars();
                 }
     
-            var text = document.createElement('span'); 
-                text.htmlFor = this.name + keys[i];
-                text.appendChild(document.createTextNode(keys[i]));
-                text.style.marginLeft = "2vh";
-                text.className = "checkmark";
+            var text = document.createElement('label');
+                text.className = "form-check-label card-text checkmark";
+                text.htmlFor = identifier;
+                text.innerHTML = keys[i];
             
             element.appendChild(checkbox);
             element.appendChild(text);
             list.appendChild(element);
         }
+
+        update_progbars();
     }
 }
 
 var streaks_template = {
     '健': false,
     '書': false,
-    '術': false,
-    '西': false,
-    '數': false,
-    '論': false,
+    '\\(7\\) Spanish Vocab': false,
+    '\\(2\\) Questions': false,
+    '\\(1\\) News': false,
+    '\\(1\\) Project': false,
+    '\\(\\frac{1}{2}\\) Paper': false,
+    'Knowledge': false,
 }
 
-var streaks = new Checklist("streaks", streaks_template, "streaklist");
+var BARPROGS = {}, BARTOTS = {};
+
+var streaks = new Checklist("streaks", streaks_template, "streaklist", "streaks_progress-bar");
 streaks.render();
